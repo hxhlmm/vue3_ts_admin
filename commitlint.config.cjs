3339584fccc88f01f11e277396969052e8971369
desire-hxh <2810351635@qq.com>
2023-06-03 13:35:16 +0800

    chore: [commitlint] add annotation

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -6,16 +6,16 @@ module.exports = {
       2,
       'always',
       [
-        'feat',
-        'fix',
-        'docs',
-        'style',
-        'refactor',
-        'perf',
-        'test',
-        'chore',
-        'revert',
-        'build',
+        'feat', // 新特性、新功能
+        'fix', // 修复Bug
+        'docs', // 文档修改
+        'style', // 代码样式修改，注意：不是CSS修改
+        'refactor', // 代码重构
+        'perf', // 优化相关，比如提升性能、体验
+        'test', // 测试用例修改
+        'chore', // 其它修改，比如改变构建流程或者增加依赖库、工具等
+        'revert', // 回滚到上一个版本
+        'build', // 编译相关的修改，例如发布版本、对项目构建或者依赖的改动
       ],
     ],
     'type-case': [0],
