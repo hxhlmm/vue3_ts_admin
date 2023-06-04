38ab590b986d2660d526cbfc2b6f4e11c0a12bbc
desire-hxh <2810351635@qq.com>
2023-06-04 18:57:40 +0800

    feat: login[1]: set up static page

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,5 @@
 <template>
   <div>
-    <h1>App根组件</h1>
-    <el-button type="primary" size="default" :icon="Plus">主要按钮</el-button>
-    <el-button type="success" size="small" :icon="Edit">编辑按钮</el-button>
-    <div>
-      <h2>1 测试svg图标的使用</h2>
-      <!-- svg是图标外层容器节点，内部需要与use标签结合使用 -->
-      <svg style="width: 100px; height: 50px">
-        <!-- xlink:herf 属性制定用哪一个图标，属性值务必 #icon- 加上svg图标名字 -->
-        <!-- use标签fill属性可以设置图标的颜色 -->
-        <use xlink:href="#icon-code" fill="red"></use>
-      </svg>
-    </div>
-    <div>
-      <h2>2 测试被封装成组件的svg图标</h2>
-      <svg-icon name="code"></svg-icon>
-      <svg-icon name="code" color="pink"></svg-icon>
-      <svg-icon name="code" color="green" width="40px" height="40px"></svg-icon>
-    </div>
-    <div>
-      <Pagination></Pagination>
-    </div>
-    <h2>3 测试能否使用sass语法</h2>
-    <h2>4 测试axios的二次封装</h2>
-    <h2>5 测试路由</h2>
     <router-view></router-view>
   </div>
 </template>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,17 +1,76 @@
 <template>
-  <div>
-    <h1>一级路由：login</h1>
+  <div class="login_container">
+    <el-row>
+      <el-col :span="8" :xs="0"></el-col>
+      <el-col :span="8" :xs="24">
+        <el-form class="login_form">
+          <h1>Hello</h1>
+          <h2>欢迎</h2>
+          <el-form-item>
+            <el-input
+              :prefix-icon="User"
+              v-model="loginForm.username"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input
+              type="password"
+              :prefix-icon="Lock"
+              show-password
+              v-model="loginForm.password"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button class="login_btn" type="primary" size="default">
+              登录
+            </el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+      <el-col :span="8" :xs="0"></el-col>
+    </el-row>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  setup() {
-    return {}
-  }
+<script setup lang="ts">
+import { User, Lock } from '@element-plus/icons-vue'
+import { reactive } from 'vue'
+// 收集账号和密码的数据
+let loginForm = reactive({
+  username: 'admin',
+  password: '12345678'
 })
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.login_container {
+  width: 100%;
+  height: 100vh;
+  background: url('@/assets/images/background.jpg') no-repeat;
+  background-size: cover;
+
+  .login_form {
+    position: relative;
+    width: 100%;
+    top: 20vh;
+    background: url('@/assets/images/login_form.png') no-repeat;
+    background-size: cover;
+    padding: 40px;
+
+    h1 {
+      color: white;
+      font-size: 40px;
+    }
+
+    h2 {
+      color: white;
+      font-size: 20px;
+      margin: 20px 0px;
+    }
+
+    .login_btn {
+      width: 100%;
+    }
+  }
+}
+</style>
